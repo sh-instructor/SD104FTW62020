21a44a4f42e17fe787d7aa2faaddf8430ed7938a
Shabbab Haider <shancoraed@outlook.com>
2020-06-09 13:12:34 -0500

    Lesson1

---
+++ ConsoleAppLesson1/ConsoleAppLesson1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is text");
+            Console.WriteLine("Hello World");
+        }
+    }
+}
