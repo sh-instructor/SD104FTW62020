63a881d21ab0635c05620e591a812def8fb8844a
Shabbab Haider <shancoraed@outlook.com>
2020-06-16 13:51:42 -0500

    changed the bad word

--- ConsoleAppLab3-5/ConsoleAppLab3-5/Program.cs
+++ ConsoleAppLab3-5/ConsoleAppLab3-5/Program.cs
@@ -6,9 +6,19 @@ namespace ConsoleAppLab3_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            // Comemnt
+            string blackListWord = "bad";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+            }
         }
     }
 }
